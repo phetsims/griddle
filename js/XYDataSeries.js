b34c2cbc5605c124d51ee5107d70c98ee874f737
jbphet <john.blanco@colorado.edu>
2015-09-14 12:13:02 -0600

    Changed ES5 getter to be a regular function.  This improved the performance on iPad significantly, though I'm not entirely sure why.  See https://github.com/phetsims/neuron/issues/55.

--- js/XYDataSeries.js
+++ js/XYDataSeries.js
@@ -13,54 +13,72 @@ define( function( require ) {
 
   function XYDataSeries( options ) {
 
-    this.options = _.extend( {
-      color: 'black'
+    options = _.extend( {
+      color: 'black',
+      maxPoints: 1000
     }, options );
 
-    this.color = this.options.color;
+    this.color = options.color; // @public
+    this.maxPoints = options.maxPoints; // @private
+    this.listeners = []; // @private
 
-    this.listeners = [];
+    //this.xPoints = [];
+    this.xPoints = new Array( options.maxPoints ); // @private
+    //this.yPoints = [];
+    this.yPoints = new Array( options.maxPoints ); // @private
 
-    //todo: preallocate?  Or use Array[Vector2] that is preallocated?
-    this.xPoints = [];
-    this.yPoints = [];
+    this.dataSeriesLength = 0; // @private, index to next available slot
 
     Events.call( this );
   }
 
   return inherit( Events, XYDataSeries, {
+
     addDataSeriesListener: function( listener ) {
       this.listeners.push( listener );
     },
+
     removeDataSeriesListener: function( listener ) {
       var index = this.listeners.indexOf( listener );
       if ( index !== -1 ) {
         this.listeners.splice( index, 1 );
       }
     },
+
     addPoint: function( x, y ) {
-      this.xPoints.push( x );
-      this.yPoints.push( y );
+
+      // store the data
+      this.xPoints[ this.dataSeriesLength ] = x;
+      this.yPoints[ this.dataSeriesLength ] = y;
+
+      // notify listeners - note that the previous data series values can be undefined in the notification
       for ( var i = 0; i < this.listeners.length; i++ ) {
-        this.listeners[ i ]( x, y, this.xPoints[ this.xPoints.length - 2 ], this.yPoints[ this.yPoints.length - 2 ] );
+        this.listeners[ i ]( x, y, this.xPoints[ this.dataSeriesLength ], this.yPoints[ this.dataSeriesLength ] );
       }
+
+      // point to the next slot
+      this.dataSeriesLength++;
+      assert && assert( this.dataSeriesLength < this.maxPoints, 'Capacity of data series exceeded' );
     },
+
     clear: function() {
-      this.xPoints = [];
-      this.yPoints = [];
+      this.dataSeriesLength = 0;
       this.trigger( 'cleared' );
     },
 
     getX: function( index ) {
-      if ( index > this.xPoints.length - 1 ) {
+      if ( index > this.dataSeriesLength - 1 ) {
         throw new Error( "No Data Point Exist at this index " + index );
       }
       return this.xPoints[ index ];
-
     },
 
-    get length() {
-      return this.xPoints.length;
+    /**
+     * @public - getter for the length.  DON'T CHANGE THIS TO AN ES5 GETTER.  That's what is was originally, and it
+     * caused poor performance on iPad, see https://github.com/phetsims/neuron/issues/55.
+     */
+    getLength: function() {
+      return this.dataSeriesLength;
     }
   } );
 } );

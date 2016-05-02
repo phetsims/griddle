5302055c8b5f267361c2307f0ae1f13247375d7a
Chris Malley <cmalley@pixelzoom.com>
2016-05-02 14:32:18 -0600

    format

--- js/XYDataSeriesNode.js
+++ js/XYDataSeriesNode.js
@@ -2,44 +2,42 @@
 
 /**
  *
- *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Sharfudeen Ashraf (for Ghent University)
  */
 define( function( require ) {
-    'use strict';
+  'use strict';
 
-    // modules
-    var inherit = require( 'PHET_CORE/inherit' );
-    var Node = require( 'SCENERY/nodes/Node' );
-    var Line = require( 'SCENERY/nodes/Line' );
-    var griddle = require( 'GRIDDLE/griddle' );
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Line = require( 'SCENERY/nodes/Line' );
+  var griddle = require( 'GRIDDLE/griddle' );
+
+  /**
+   *
+   * @constructor
+   */
+  function XYDataSeriesNode( xyDataSeries, options ) {
+    var xyDataSeriesNode = this;
+    Node.call( this, options );
+
+    var listener = function( x, y, xPrevious, yPrevious ) {
+      if ( xPrevious && yPrevious && (xPrevious !== 0 || yPrevious !== 0 ) ) {
+        xyDataSeriesNode.addChild( new Line( xPrevious, yPrevious, x, y, { stroke: xyDataSeries.color } ) );
+      }
+    };
+    xyDataSeries.addDataSeriesListener( listener );
 
     /**
-     *
-     * @constructor
+     * @public
      */
-    function XYDataSeriesNode( xyDataSeries, options ) {
-      var xyDataSeriesNode = this;
-      Node.call( this, options );
-
-      var listener = function( x, y, xPrevious, yPrevious ) {
-        if ( xPrevious && yPrevious && (xPrevious !== 0 || yPrevious !== 0 ) ) {
-          xyDataSeriesNode.addChild( new Line( xPrevious, yPrevious, x, y, { stroke: xyDataSeries.color } ) );
-        }
-      };
-      xyDataSeries.addDataSeriesListener( listener );
-
-      /**
-       * @public
-       */
-      this.dispose = function() {
-        xyDataSeries.removeDataSeriesListener( listener );
-      };
-    }
-
-    griddle.register( 'XYDataSeriesNode', XYDataSeriesNode );
-
-    return inherit( Node, XYDataSeriesNode );
+    this.dispose = function() {
+      xyDataSeries.removeDataSeriesListener( listener );
+    };
   }
-);
+
+  griddle.register( 'XYDataSeriesNode', XYDataSeriesNode );
+
+  return inherit( Node, XYDataSeriesNode );
+} );

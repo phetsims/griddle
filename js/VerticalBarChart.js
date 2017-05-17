e9d7495a64fcef2b580061b3df0db7ecefb52f46
Denz1994 <denz1994@gmail.com>
2017-05-17 16:03:29 -0600

    Added some fundamental elements to the graph such as background, and y-axis.

--- js/VerticalBarChart.js
+++ js/VerticalBarChart.js
@@ -9,26 +9,53 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var ArrowNode = require( 'SCENERY_PHET/ArrowNode' );
   var griddle = require( 'GRIDDLE/griddle' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Node = require( 'SCENERY/nodes/Node' );
+
+  // strings
+  var LINE_WIDTH = 0.8; // Empirically determined
+  var STROKE_COLOR = 'gray';
+  var LABEL_OFFSET = 6;
 
   /**
    * @constructor
    */
-  function VerticalBarChart( barNodes ) {
+  function VerticalBarChart( barNodes, options ) {
     var self = this;
+
+    var content = new Node();
+
+    options = _.extend( {
+      width: 400,
+      height: 400,
+      backgroundFill: 'white'
+    }, options );
     Node.call( this );
 
     // TODO: draw vertical arrow
+    this.yAxis = new ArrowNode( 0, 0, 10, 10 );
 
     // TODO: draw bars
-
-    // TODO: draw horizontal line (x axis)
+    this.rectangle = new Rectangle( 0, 200, options.width, options.height, {
+      fill: options.backgroundFill,
+      stroke: STROKE_COLOR,
+      cornerRadius: 8,
+      lineWidth: LINE_WIDTH,
+      children: [ this.yAxis ]
+    } );
+    content.addChild( this.rectangle );
 
     barNodes.forEach( function( barNode ) {
       self.addChild( barNode );
+
+      // TODO: draw horizontal line (x axis)
+      
     } );
+    this.addChild( content );
   }
 
   griddle.register( 'VerticalBarChart', VerticalBarChart );

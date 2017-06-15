221f1c7be420438c87a8547b64b7e8e7e176c0a9
Denz1994 <denz1994@gmail.com>
2017-06-15 13:32:02 -0600

    Added ability to show continuous arrows for bars that exceed bar graph. Arrow positioning still needs to be adjusted.

--- js/VerticalBarNode.js
+++ js/VerticalBarNode.js
@@ -5,7 +5,7 @@
  * @author Denzell Barnett (PhET Interactive Simulations)
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Johnathan Olson (PhET Interactive Simulations)
- * 
+ *
  */
 define( function( require ) {
   'use strict';
@@ -15,6 +15,8 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var ArrowNode = require( 'SCENERY_PHET/ArrowNode' );
+  var Property = require( 'AXON/Property' );
 
   /**
    * @constructor
@@ -26,7 +28,8 @@ define( function( require ) {
       lineWidth: 0,
       label: null, // Optional label which
       width: 30,
-      maxHeight: 400
+      maxHeight: 400,
+      displayContinuousArrow: false
     }, options );
 
     var self = this;
@@ -40,12 +43,31 @@ define( function( require ) {
       lineWidth: options.lineWidth
     } );
 
+    // @public arrow node used to indicate when the value has gone beyond the scale of this meter
+    this.arrowNode = new ArrowNode( 0, 0, 10 + 2, 0, {
+      fill: options.fill,
+      headWidth: options.width,
+      tailWidth: 12,
+      stroke: 'black'
+    } );
+    this.addChild( this.arrowNode );
+
+    var showContinuousArrow = new Property( options.displayContinuousArrow );
+
     this.addChild( this.rectangleNode );
     property.link( function( value ) {
       self.rectangleNode.visible = ( value > 0 ); // because we can't create a zero height rectangle
       var height = Math.max( 1, value ); // bar must have non-zero size
       self.rectangleNode.setRectHeight( Math.min( options.maxHeight, height ) );
       self.rectangleNode.bottom = 0;
+
+      // set the continuous arrow to visible if needed
+      var currentHeight = self.rectangleNode.getRectHeight();
+      showContinuousArrow.set( currentHeight === options.maxHeight );
+    } );
+
+    showContinuousArrow.link( function( shown ) {
+      self.arrowNode.visible = shown && options.displayContinuousArrow;
     } );
     this.mutate( options );
   }

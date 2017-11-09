8543c14fbbf31fb59e33b883951fc30da28c0c29
denz1994 <denz1994@gmail.com>
2017-11-09 11:46:36 -0500

    Adjusted tandem reference and updated documentation.

--- js/VerticalBarChart.js
+++ js/VerticalBarChart.js
@@ -26,6 +26,9 @@ define( function( require ) {
   var STROKE_COLOR = 'gray';
 
   /**
+   * @param {Array.<verticalBarNode>} barNodes- array of barNodes to be added into the chart area
+   * @param {Object} options
+   *
    * @constructor
    */
   function VerticalBarChart( barNodes, options ) {

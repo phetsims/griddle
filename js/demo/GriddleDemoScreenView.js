32bf9c0cab210523ed744538e303c737d3b66f5e
samreid <reids@colorado.edu>
2018-09-24 12:09:02 -0600

    Moved GriddleDemoScreenView to ES6, see https://github.com/phetsims/griddle/issues/30

--- js/demo/GriddleDemoScreenView.js
+++ js/demo/GriddleDemoScreenView.js
@@ -8,74 +8,81 @@
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Chris Malley (PixelZoom, Inc.)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
   const LabeledScrollingChartNode = require( 'GRIDDLE/LabeledScrollingChartNode' );
   const ScrollingChartNode = require( 'GRIDDLE/ScrollingChartNode' );
   const Text = require( 'SCENERY/nodes/Text' );
-  var BarChartNode = require( 'GRIDDLE/BarChartNode' );
-  var Color = require( 'SCENERY/util/Color' );
-  var DemosScreenView = require( 'SUN/demo/DemosScreenView' );
-  var Dimension2 = require( 'DOT/Dimension2' );
-  var Emitter = require( 'AXON/Emitter' );
-  var griddle = require( 'GRIDDLE/griddle' );
-  var HBox = require( 'SCENERY/nodes/HBox' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var Node = require( 'SCENERY/nodes/Node' );
-  var Panel = require( 'SUN/Panel' );
-  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var Property = require( 'AXON/Property' );
-  var Range = require( 'DOT/Range' );
-  var sceneryPhetQueryParameters = require( 'SCENERY_PHET/sceneryPhetQueryParameters' );
-  var Vector2 = require( 'DOT/Vector2' );
-  var VSlider = require( 'SUN/VSlider' );
-  var XYDataSeries = require( 'GRIDDLE/XYDataSeries' );
-  var XYPlot = require( 'GRIDDLE/XYPlot' );
+  const BarChartNode = require( 'GRIDDLE/BarChartNode' );
+  const Color = require( 'SCENERY/util/Color' );
+  const DemosScreenView = require( 'SUN/demo/DemosScreenView' );
+  const Dimension2 = require( 'DOT/Dimension2' );
+  const Emitter = require( 'AXON/Emitter' );
+  const griddle = require( 'GRIDDLE/griddle' );
+  const HBox = require( 'SCENERY/nodes/HBox' );
+  const Node = require( 'SCENERY/nodes/Node' );
+  const Panel = require( 'SUN/Panel' );
+  const PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  const Property = require( 'AXON/Property' );
+  const Range = require( 'DOT/Range' );
+  const sceneryPhetQueryParameters = require( 'SCENERY_PHET/sceneryPhetQueryParameters' );
+  const Vector2 = require( 'DOT/Vector2' );
+  const VSlider = require( 'SUN/VSlider' );
+  const XYDataSeries = require( 'GRIDDLE/XYDataSeries' );
+  const XYPlot = require( 'GRIDDLE/XYPlot' );
 
   // constants - this is a hack to enable components to animate from the animation loop
-  var emitter = new Emitter();
+  const emitter = new Emitter();
 
-  /**
-   * @constructor
-   */
-  function GriddleDemoScreenView() {
+  class GriddleDemoScreenView extends DemosScreenView {
+    constructor() {
 
-    DemosScreenView.call( this, [
+      super( [
 
-      /**
-       * To add a demo, add an object literal here. Each object has these properties:
-       *
-       * {string} label - label in the combo box
-       * {function(Bounds2): Node} createNode - creates the scene graph for the demo
-       */
-      { label: 'XYPlot', createNode: demoXYPlot },
-      { label: 'BarChart', createNode: demoBarChart },
-      { label: 'ScrollingChartNode', createNode: demoScrollingChartNode }
-    ], {
-      comboBoxItemFont: new PhetFont( 12 ),
-      comboBoxItemYMargin: 3,
-      selectedDemoLabel: sceneryPhetQueryParameters.component
-    } );
+        /**
+         * To add a demo, add an object literal here. Each object has these properties:
+         *
+         * {string} label - label in the combo box
+         * {function(Bounds2): Node} createNode - creates the scene graph for the demo
+         */
+        { label: 'XYPlot', createNode: demoXYPlot },
+        { label: 'BarChart', createNode: demoBarChart },
+        { label: 'LabeledScrollingChartNode', createNode: demoLabeledScrollingChartNode }
+      ], {
+        comboBoxItemFont: new PhetFont( 12 ),
+        comboBoxItemYMargin: 3,
+        selectedDemoLabel: sceneryPhetQueryParameters.component
+      } );
+    }
+
+    /**
+     * Move the model forward in time.
+     * @param {number} dt - elapsed time in seconds
+     * @public
+     */
+    step( dt ) {
+      emitter.emit1( dt );
+    }
   }
 
   // Creates a demo for the XYPlot
-  var demoXYPlot = function( layoutBounds ) {
+  const demoXYPlot = function( layoutBounds ) {
 
-    var time = 0;
-    var plot = new XYPlot( { backgroundFill: '#efecd9' } );
-    var plotPanel = new Panel( plot, {
+    let time = 0;
+    const plot = new XYPlot( { backgroundFill: '#efecd9' } );
+    const plotPanel = new Panel( plot, {
       fill: '#efecd9',
       xMargin: 10,
       yMargin: 10,
       x: 100,
       y: 100
     } );
-    var series = new XYDataSeries( { color: Color.BLUE } );
+    const series = new XYDataSeries( { color: Color.BLUE } );
     plot.addSeries( series, false );
-    var forward = true;
-    var count = 0;
+    let forward = true;
+    let count = 0;
     const listener = function( dt ) {
       series.addPoint( time, -Math.abs( -Math.sin( time / 100 + count ) * 400 * 0.8 ) );
       time = time + ( forward ? 1 : -1 );
@@ -92,7 +99,7 @@ define( function( require ) {
     emitter.addListener( listener );
 
     // Swap out the dispose function for one that also removes the Emitter listener
-    var plotPanelDispose = plotPanel.dispose.bind( plotPanel );
+    const plotPanelDispose = plotPanel.dispose.bind( plotPanel );
     plotPanel.dispose = function() {
       emitter.removeListener( listener );
       plotPanelDispose();
@@ -101,26 +108,26 @@ define( function( require ) {
   };
 
   // Creates a demo for the BarChartNode
-  var demoBarChart = function( layoutBounds ) {
-    var model = {
+  const demoBarChart = function( layoutBounds ) {
+    const model = {
       aProperty: new Property( 0 ),
       bProperty: new Property( 0 ),
       cProperty: new Property( 0 )
     };
-    var aEntry = {
+    const aEntry = {
       property: model.aProperty,
       color: 'red'
     };
-    var bEntry = {
+    const bEntry = {
       property: model.bProperty,
       color: 'green'
     };
-    var cEntry = {
+    const cEntry = {
       property: model.cProperty,
       color: 'blue'
     };
 
-    var barChartNode = new BarChartNode( [
+    const barChartNode = new BarChartNode( [
       { entries: [ aEntry ], label: new Node() },
       { entries: [ bEntry ], label: new Node() },
       { entries: [ cEntry ], label: new Node() },
@@ -158,7 +165,7 @@ define( function( require ) {
     } );
 
     // Swap out the dispose function for one that also removes the Emitter listener
-    var hboxDispose = hBox.dispose.bind( hBox );
+    const hboxDispose = hBox.dispose.bind( hBox );
     hBox.dispose = function() {
       emitter.removeListener( listener );
       hboxDispose();
@@ -166,18 +173,27 @@ define( function( require ) {
     return hBox;
   };
 
-  var demoScrollingChartNode = function( layoutBounds ) {
-
-    var timeProperty = new Property( 0 );
-    var series1 = {
+  /**
+   * Creates an example LabeledScrollingChartNode
+   * @param layoutBounds
+   */
+  const demoLabeledScrollingChartNode = function( layoutBounds ) {
+    const timeProperty = new Property( 0 );
+    const series1 = {
       color: 'blue',
       data: [],
       emitter: emitter
     };
-    var maxSeconds = 4;
-    const listener = function( dt ) {
+    const maxSeconds = 4;
+    const listener = dt => {
+
+      // Increment the model time
       timeProperty.value += dt;
+
+      // Sample new data
       series1.data.push( new Vector2( timeProperty.value, Math.sin( timeProperty.value ) ) );
+
+      // Data that does not fall within the displayed window should be removed.
       while ( series1.data[ 0 ].x < timeProperty.value - maxSeconds ) {
         series1.data.shift();
       }
@@ -198,19 +214,13 @@ define( function( require ) {
     } );
 
     // Swap out the dispose function for one that also removes the Emitter listener
-    var panelDispose = panel.dispose.bind( panel );
-    panel.dispose = function() {
+    const panelDispose = panel.dispose.bind( panel );
+    panel.dispose = () => {
       emitter.removeListener( listener );
       panelDispose();
     };
     return panel;
   };
 
-  griddle.register( 'GriddleDemoScreenView', GriddleDemoScreenView );
-
-  return inherit( DemosScreenView, GriddleDemoScreenView, {
-    step: function( dt ) {
-      emitter.emit1( dt );
-    }
-  } );
+  return griddle.register( 'GriddleDemoScreenView', GriddleDemoScreenView );
 } );

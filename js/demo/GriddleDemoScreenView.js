999f73587f5c184bf4abf693f5a9a5cd3db52afb
Jesse <jesse.greenberg@colorado.edu>
2020-05-27 16:34:41 -0400

    Move GridNode to griddle from scenery-phet, see phetsims/griddle#51 and phetsims/scenery-phet#601

--- js/demo/GriddleDemoScreenView.js
+++ js/demo/GriddleDemoScreenView.js
@@ -9,22 +9,30 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
+import BooleanProperty from '../../../axon/js/BooleanProperty.js';
 import Emitter from '../../../axon/js/Emitter.js';
+import NumberProperty from '../../../axon/js/NumberProperty.js';
 import Property from '../../../axon/js/Property.js';
 import Dimension2 from '../../../dot/js/Dimension2.js';
 import Range from '../../../dot/js/Range.js';
 import merge from '../../../phet-core/js/merge.js';
+import PhetFont from '../../../scenery-phet/js/PhetFont.js';
 import sceneryPhetQueryParameters from '../../../scenery-phet/js/sceneryPhetQueryParameters.js';
 import HBox from '../../../scenery/js/nodes/HBox.js';
 import Node from '../../../scenery/js/nodes/Node.js';
 import Text from '../../../scenery/js/nodes/Text.js';
+import VBox from '../../../scenery/js/nodes/VBox.js';
 import Color from '../../../scenery/js/util/Color.js';
+import BooleanRectangularStickyToggleButton from '../../../sun/js/buttons/BooleanRectangularStickyToggleButton.js';
+import BooleanRectangularToggleButton from '../../../sun/js/buttons/BooleanRectangularToggleButton.js';
 import DemosScreenView from '../../../sun/js/demo/DemosScreenView.js';
+import NumberSpinner from '../../../sun/js/NumberSpinner.js';
 import Panel from '../../../sun/js/Panel.js';
 import VSlider from '../../../sun/js/VSlider.js';
 import BarChartNode from '../BarChartNode.js';
 import DynamicSeries from '../DynamicSeries.js';
 import griddle from '../griddle.js';
+import GridNode from '../GridNode.js';
 import ScrollingChartNode from '../ScrollingChartNode.js';
 import XYPlot from '../XYPlot.js';
 
@@ -43,6 +51,7 @@ class GriddleDemoScreenView extends DemosScreenView {
        * {function(Bounds2): Node} createNode - creates the scene graph for the demo
        */
       { label: 'XYPlot', createNode: demoXYPlot },
+      { label: 'GridNode', createNode: demoGridNode },
       { label: 'BarChart', createNode: demoBarChart },
       { label: 'ScrollingChartNode', createNode: demoScrollingChartNode }
     ], {
@@ -163,6 +172,109 @@ const demoBarChart = function( layoutBounds ) {
     hboxDispose();
   };
   return hBox;
+};
+
+// Creates a demo for GridNode
+const demoGridNode = layoutBounds => {
+  const gridWidth = 360;
+  const gridHeight = 360;
+  const minorSpacingRange = new Range( 10, 40 );
+  const majorSpacingRange = new Range( 120, 360 );
+  const defaultMinorSpacing = minorSpacingRange.min;
+  const defaultMajorSpacing = majorSpacingRange.min;
+
+  const gridNode = new GridNode( gridWidth, gridHeight, {
+    majorHorizontalLineSpacing: defaultMajorSpacing,
+    majorVerticalLineSpacing: defaultMajorSpacing,
+    minorHorizontalLineSpacing: defaultMinorSpacing,
+    minorVerticalLineSpacing: defaultMinorSpacing
+  } );
+
+  // creates a NumberSpinner with a text label that controls grid spacing
+  const createLabelledSpinner = ( labelString, numberProperty, enabledProperty, valueDelta ) => {
+    const label = new Text( labelString, { font: new PhetFont( 15 ) } );
+    const spinner = new NumberSpinner( numberProperty, new Property( numberProperty.range ), {
+      deltaValue: valueDelta,
+      enabledProperty: enabledProperty
+    } );
+    return new HBox( {
+      children: [ label, spinner ],
+      spacing: 5
+    } );
+  };
+
+  // creates a BooleanRectangularToggleButton that toggles visibility of grid lines
+  const createToggleLinesButton = ( visibleProperty, visibleText, hiddenText ) => {
+    return new BooleanRectangularToggleButton( new Text( visibleText ), new Text( hiddenText ), visibleProperty );
+  };
+
+  // Properties for controls to change the GridNode
+  const verticalLinesVisibleProperty = new BooleanProperty( true );
+  const horizontalLinesVisibleProperty = new BooleanProperty( true );
+  const scrollingProperty = new BooleanProperty( false );
+
+  const minorHorizontalLineSpacingProperty = new NumberProperty( defaultMinorSpacing, { range: minorSpacingRange } );
+  const minorVerticalLineSpacingProperty = new NumberProperty( defaultMinorSpacing, { range: minorSpacingRange } );
+  const majorHorizontalLineSpacingProperty = new NumberProperty( defaultMajorSpacing, { range: majorSpacingRange } );
+  const majorVerticalLineSpacingProperty = new NumberProperty( defaultMajorSpacing, { range: majorSpacingRange } );
+
+  // controls to change the GridNode
+  const minorHorizontalLineSpinner = createLabelledSpinner( 'Minor Horizontal Spacing', minorHorizontalLineSpacingProperty, horizontalLinesVisibleProperty, 10 );
+  const minorVerticalLineSpinner = createLabelledSpinner( 'Minor Vertical Spacing', minorVerticalLineSpacingProperty, verticalLinesVisibleProperty, 10 );
+  const majorHorizontalLineSpinner = createLabelledSpinner( 'Major Horizontal Spacing', majorHorizontalLineSpacingProperty, horizontalLinesVisibleProperty, 120 );
+  const majorVerticalLineSpinner = createLabelledSpinner( 'Major Vertical Spacing', majorVerticalLineSpacingProperty, verticalLinesVisibleProperty, 120 );
+
+  const hideHorizontalLinesButton = createToggleLinesButton( horizontalLinesVisibleProperty, 'Hide Horizontal', 'Show Horizontal' );
+  const hideVerticalLinesButton = createToggleLinesButton( verticalLinesVisibleProperty, 'Hide Vertical', 'Show Horizontal' );
+
+  const hideButtonsHBox = new HBox( {
+    children: [ hideHorizontalLinesButton, hideVerticalLinesButton ],
+    spacing: 10
+  } );
+
+  const scrollButton = new BooleanRectangularStickyToggleButton( scrollingProperty, {
+    content: new Text( 'Scroll' )
+  } );
+
+  const controls = new VBox( {
+    children: [ minorHorizontalLineSpinner, minorVerticalLineSpinner, majorHorizontalLineSpinner, majorVerticalLineSpinner, hideButtonsHBox, scrollButton ],
+    spacing: 15,
+    align: 'right'
+  } );
+
+  const node = new HBox( {
+    children: [ controls, gridNode ],
+    spacing: 15,
+    center: layoutBounds.center,
+    resize: false
+  } );
+
+  Property.multilink( [ verticalLinesVisibleProperty, horizontalLinesVisibleProperty ], ( verticalVisible, horizontalVisible ) => {
+    const majorVerticalLineSpacing = verticalVisible ? majorVerticalLineSpacingProperty.get() : null;
+    const minorVerticalLineSpacing = verticalVisible ? minorVerticalLineSpacingProperty.get() : null;
+    const majorHorizontalLineSpacing = horizontalVisible ? majorHorizontalLineSpacingProperty.get() : null;
+    const minorHorizontalLineSpacing = horizontalVisible ? minorHorizontalLineSpacingProperty.get() : null;
+
+    gridNode.setLineSpacings( majorVerticalLineSpacing, majorHorizontalLineSpacing, minorVerticalLineSpacing, minorHorizontalLineSpacing );
+
+    // disable the other button, cant have both sets hidden at once
+    hideHorizontalLinesButton.enabled = verticalVisible;
+    hideVerticalLinesButton.enabled = horizontalVisible;
+  } );
+
+  Property.multilink( [ majorVerticalLineSpacingProperty, majorHorizontalLineSpacingProperty, minorHorizontalLineSpacingProperty, minorVerticalLineSpacingProperty ],
+    gridNode.setLineSpacings.bind( gridNode )
+  );
+
+  emitter.addListener( dt => {
+    if ( scrollingProperty.get() ) {
+      const offset = gridNode.verticalLineOffset + 80 * dt;
+      gridNode.verticalLineOffset = offset;
+      gridNode.horizontalLineOffset = offset;
+    }
+  } );
+
+  return node;
 };
 
 /**

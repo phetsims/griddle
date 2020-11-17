1cce18b0313cba3abcc5d4920cc301dd306249f6
samreid <reids@colorado.edu>
2020-11-16 22:28:07 -0700

    Stop drawing for NaN, see https://github.com/phetsims/griddle/issues/86

--- js/bamboo/LinePlot.js
+++ js/bamboo/LinePlot.js
@@ -14,14 +14,35 @@ class LinePlot extends Path {
   constructor( chartModel, data, options ) {
     super( null, options );
 
-    chartModel.link( () => {
-      const shape = new Shape();
-      for ( let i = 0; i < data.length; i++ ) {
-        i === 0 && shape.moveToPoint( chartModel.modelToViewPosition( data[ i ] ) );
-        i !== 0 && shape.lineToPoint( chartModel.modelToViewPosition( data[ i ] ) );
+    // @private
+    this.chartModel = chartModel;
+    this.data = data;
+    chartModel.link( () => this.update() );
+  }
+
+  // @public
+  // TODO: renders 2x/frame if a data point is added and the chart scrolls
+  update() {
+    const shape = new Shape();
+    let moveToNextPoint = true;
+    for ( let i = 0; i < this.data.length; i++ ) {
+
+      // NaN or Infinite components "pen up" and stop drawing
+      if ( this.data[ i ].isFinite() ) {
+        const viewPoint = this.chartModel.modelToViewPosition( this.data[ i ] );
+        if ( moveToNextPoint ) {
+          shape.moveToPoint( viewPoint );
+          moveToNextPoint = false;
+        }
+        else {
+          shape.lineToPoint( viewPoint );
+        }
       }
-      this.shape = shape;
-    } );
+      else {
+        moveToNextPoint = true;
+      }
+    }
+    this.shape = shape;
   }
 }
 

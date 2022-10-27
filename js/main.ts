cf43bdfe82817ab14a97cb97ef75a19a843278da
Jonathan Olson <jonathan.olson@colorado.edu>
2022-10-27 10:45:09 -0600

    Updating phet-lib with main common code references.

---
+++ js/main.ts
@@ -0,0 +1,20 @@
+// Copyright 2022, University of Colorado Boulder
+
+import griddle from './griddle.js';
+
+import './BarChartNode.js';
+import './BarNode.js';
+import './DynamicSeries.js';
+import './DynamicSeriesNode.js';
+import './GridNode.js';
+import './GriddleStrings.js';
+import './GriddleUtils.js';
+import './PointStyle.js';
+import './PointStyledVector2.js';
+import './SeismographDynamicSeriesNode.js';
+import './SeismographNode.js';
+import './SpanNode.js';
+import './XYChartNode.js';
+import './XYCursorChartNode.js';
+
+export default griddle;

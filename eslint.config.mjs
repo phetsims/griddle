499e447785142a904efcb6457c9876b88d4dc285
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:34 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for griddle.
+ * ESLint configuration for griddle.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)

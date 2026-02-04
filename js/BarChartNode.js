9c3f2d79ed255b9e358d7e0a878125f37ccf736e
phet-dev <phetadmin@gmail.com>
2026-02-04 04:58:44 -0700

    update copyright dates from daily grunt work

--- js/BarChartNode.js
+++ js/BarChartNode.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2025, University of Colorado Boulder
+// Copyright 2017-2026, University of Colorado Boulder
 
 /**
  * Bar chart for sims that need to display separate values. This chart is vertically aligned, with bars that represent
